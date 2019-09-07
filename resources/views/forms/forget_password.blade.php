247c2b38df1ec42870b1f7087b70d77686a37174
Mohamed Yousef <modi401@hotmail.com>
2019-09-07 08:34:22 +0200

    working on forget password

---
+++ resources/views/forms/forget_password.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('title', $title)
+
+@section('container')
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 text-center white-box">
+            <h1>Forget Password</h1>
+            <p id="hint">Enter your Email , and you will recieve an email with a new password.</p>
+
+            <br>
+            
+            @if ($errors->any())
+                <div class="alert alert-danger text-left" role="alert">
+                    @foreach ($errors->all() as $error)
+                        {{ $error }}
+                    @endforeach
+                </div>  
+            @endif
+            
+            <br>
+
+            <form method="post" action="{{ route('reset_password') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" id="InputEmail" placeholder="Enter Your Email">
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn">OK</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+@endsection
