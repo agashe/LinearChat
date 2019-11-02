14d414d13db8cc24fae38c2f9824e2a96dbcc3b6
Mohamed Yousef <modi401@hotmail.com>
2019-11-02 10:21:36 +0200

    Chat board front done. To back :)

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,19 +6,22 @@
     <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6 chat-board">
-            @for ($i = 0;$i < 5;$i++)
-                <div class="white-box message">
-                    <img src="{{ asset('image/avatar.jpg') }}" /> Username
-                    <h5>25/8/2019 - 12:05:23 PM</h5>
-                    <article>
-                        test testtest testtest testtest testtest testtest test
-                        test testtest testtest testtest testtest testtest test
-                        test testtest testtest testtest testtest testtest test
-                        test testtest testtest testtest testtest testtest test
-                        test testtest testtest testtest testtest testtest test
-                    </article>
-                </div>
-            @endfor
+            <div class="chat-board-content">
+                @for ($i = 0;$i < 20;$i++)
+                    <div class="white-box message">
+                        <img src="{{ asset('image/avatar.jpg') }}" /> Username
+                        <h5>25/8/2019 - 12:05:23 PM</h5>
+                        <article>
+                            test testtest testtest testtest testtest testtest test
+                        </article>
+                    </div>
+                @endfor
+            </div>
+
+            <div class="message-box">
+                <input type="text" name="message" placeholder="Write Something...">
+                <i class="icon icon-play" id="send-message" title="SEND!"></i>
+            </div>
         </div>
         <div class="col-md-3">
             <a href="{{ route('logout') }}" class="btn btn-circle icon icon-signout" title="Log Out"></a>
