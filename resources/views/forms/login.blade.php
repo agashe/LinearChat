a04269417674d2b9692f1038cf3ad747ca2fb63d
Mohamed Yousef <modi401@hotmail.com>
2020-07-21 14:14:53 +0200

    fix login form

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -4,12 +4,9 @@
 
 @section('container')
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 text-center white-box">
+        <div class="col-md-6 offset-md-3 text-center white-box">
             <h1><span class="red">LOG</span> <span class="blue">IN</span></h1>
             <p id="hint">Sign In with your account to start chating</p>
-
-            <br>
             
             @if ($errors->any())
                 <div class="alert alert-danger text-left" role="alert">
@@ -18,24 +15,20 @@
                     @endforeach
                 </div>  
             @endif
-            
-            <br>
 
-            <form method="post" action="{{ route('check_user') }}">
+            <form method="post" action="{{ route('check_user') }}" class="my-3">
                 {{ csrf_field() }}
 
-                <div class="form-group">
+                <div class="form-group mt-3">
                     <input type="email" name="email" class="form-control" id="InputEmail" placeholder="Enter Your Email">
                 </div>
                 <div class="form-group">
                     <input type="password" name="password" class="form-control" id="InputPassword" placeholder="Enter Your Password">
                 </div>
 
-                <div class="row">
-                    <a href="{{ route('register') }}" class="hint blue">Don't have account</a>
-                    <br>
-                    <a href="{{ route('forget_password') }}" class="hint blue">Forget your password</a>
-                    <br><br>
+                <div class="text-center">
+                    <p class="mb-0"><a href="{{ route('register') }}" class="hint blue">Don't have account</a></p>
+                    <p class="mb-4"><a href="{{ route('forget_password') }}" class="hint blue">Forget your password</a></p>
                 </div>
                 
                 <div class="row">
@@ -45,6 +38,5 @@
                 </div>
             </form>
         </div>
-        <div class="col-md-3"></div>
     </div>
 @endsection
